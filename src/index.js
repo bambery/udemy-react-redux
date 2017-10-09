e8b252ec3f3e0dc578773b862484ab7d7ddb47c8
bambery <lwszolek@gmail.com>
2017-10-08 21:39:02 -0700

    completed lesson 25 - iterating through list item components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import Secret from './secret';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list.js'
 
-const App = () => {
-  return ( 
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({key: Secret.youtube, term: 'cats'}, (videos) => {
+      this.setState({ videos }); 
+    });
+  }
+
+  render(){
+    return ( 
     <div>
       <SearchBar />
+      <VideoList videos={ this.state.videos }/>
     </div>
-  );
+    );
+  }
 };
 
 ReactDOM.render( <App />, document.querySelector('.container') );
