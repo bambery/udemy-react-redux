1ca5ac7036556ba34ce2fa98c0eff56d0efb3637
bambery <lwszolek@gmail.com>
2017-10-15 12:41:09 -0700

    Completed lesson 29 - selecting/clicking video from list

--- src/index.js
+++ src/index.js
@@ -11,9 +11,15 @@ import VideoDetail from './components/video_detail.js';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
     YTSearch({key: Secret.youtube, term: 'cats'}, (videos) => {
-      this.setState({ videos }); 
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      }); 
     });
   }
 
@@ -21,8 +27,10 @@ class App extends Component {
     return ( 
     <div>
       <SearchBar />
-      <VideoDetail video={this.state.videos[0]} />
-      <VideoList videos={ this.state.videos }/>
+      <VideoDetail video={ this.state.selectedVideo }/>
+      <VideoList 
+        onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+        videos={ this.state.videos } />
     </div>
     );
   }
